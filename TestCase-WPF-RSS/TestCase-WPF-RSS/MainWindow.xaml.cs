7fd3f42a83bc2d91cb1d18ba9d2a2e87124924bc
Dmitriy Sidyakin <sidyakind@gmail.com>
2023-05-11 15:43:25 +0300

    Добавил форму загрузки

--- TestCase-WPF-RSS/TestCase-WPF-RSS/MainWindow.xaml.cs
+++ TestCase-WPF-RSS/TestCase-WPF-RSS/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,6 +25,17 @@ namespace TestCase_WPF_RSS
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var width = LoadingText.ActualWidth;
+            DoubleAnimation textAnimation = new DoubleAnimation();
+            textAnimation.From = width;
+            textAnimation.To = 200;
+            textAnimation.Duration = TimeSpan.FromSeconds(3);
+            textAnimation.RepeatBehavior = new RepeatBehavior(64000);
+            LoadingText.BeginAnimation(Label.WidthProperty, textAnimation);
         }
     }
 }
